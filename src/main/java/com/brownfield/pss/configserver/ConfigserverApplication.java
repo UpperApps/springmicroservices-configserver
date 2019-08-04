c8f055e577990f31626a69f9a41528aee9833f97
Rodrigo Melo <rodrigo.melo@skipthedishes.ca>
2019-08-04 16:50:46 -0500

    First commit.

---
+++ src/main/java/com/brownfield/pss/configserver/ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.brownfield.pss.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
