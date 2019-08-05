0ef9b1ad28ff09f1cc819e50ffc31f5313f8f613
Rodrigo Melo <rodrigo.melo@skipthedishes.ca>
2019-08-05 12:07:39 -0500

    Actuator configuration.

---
+++ src/main/java/com/brownfield/pss/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.brownfield.pss.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
